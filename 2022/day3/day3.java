ea2568019052412de68474bdeb8b9f162705f0ce
Zsargul <kubakerlino@gmail.com>
2022-12-31 21:03:56 +0000

    day 3

--- 2022/day3/day3.java
+++ 2022/day3/day3.java
@@ -8,7 +8,7 @@ class Day3 {
 		}
 
 		char c = '\0';
-		for (char ch: firstHalf.toCharArray()) {
+		for (char ch : firstHalf.toCharArray()) {
 			if (secondHalf.indexOf(ch) != -1) {
 				c = ch;				
 			}
@@ -16,6 +16,16 @@ class Day3 {
 		return c;
 	}
 
+	public static char getCommonChar2(String one, String two, String three) {
+		char c = '\0';
+		for (char ch : one.toCharArray()) {
+			if (two.contains(Character.toString(ch)) && three.contains(Character.toString(ch))) {
+				c = ch;
+			}
+		}
+		return c;
+	}
+	
 	public static int getCharValue(char c) {
 		int val = c;
 		if (val >= 97 && val <= 122) { // Lowercase
@@ -46,6 +56,32 @@ class Day3 {
 				sum += getCharValue(c);
 			}
 			System.out.println(sum); // Part 1
+
+			// Gather up linse into groups of 3
+			if (lines.size() % 3 != 0) {
+				throw new RuntimeException("The number of lines is not a multiple of 3");
+			}
+
+			ArrayList<ArrayList<String>> groups = new ArrayList<ArrayList<String>>();
+			for (int i = 0; i < lines.size(); i += 3) {
+				ArrayList<String> group	= new ArrayList<String>();
+				group.add(lines.get(i));
+				group.add(lines.get(i+1));
+				group.add(lines.get(i+2));
+				groups.add(group);
+			}
+
+			// Find common characters
+			sum = 0;
+			for (ArrayList group : groups) {
+				String a = group.get(0).toString();
+				String b = group.get(1).toString();
+				String c = group.get(2).toString();
+				char common = getCommonChar2(a, b, c);
+				sum += getCharValue(common);	
+			}
+			System.out.println(sum); // Part 2
+
 		} catch (IOException e) {
 			throw new RuntimeException(e);
 		}
